dad5971d6b514b9ac95b1571fb69b446ab4e5583
Apoorva <jainapoorva40@gmail.com>
2021-07-03 17:07:34 +0530

    Added my second project

---
+++ verlet1.js
@@ -0,0 +1,15 @@
+var x=5;
+document.write(x,"\n");
+
+//calling y after declaration
+
+let y=10;
+document.write(y,"\n");
+
+//calling var z before defination will return undefined
+document.write(z,"\n");
+var z=2;
+
+//calling let a before defination will give error
+document.write(a);
+let a=3;
